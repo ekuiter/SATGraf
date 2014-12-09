12fdae1d58c140cc2fc030440b34de9c16eaaaa8
William Lindsay <william.fr.lindsay@gmail.com>
2014-12-08 21:03:29 -0500

    - Added Jimmy's test file - Increased drawing performance for edges

--- src/com/satgraf/community/UI/CommunityCanvas.java
+++ src/com/satgraf/community/UI/CommunityCanvas.java
@@ -36,45 +36,49 @@ public class CommunityCanvas extends GraphCanvas{
   @Override
   public void paintThread(PaintThread paint) {
 	Rectangle o = paint.getBounds();
-    List<Node> nodes = graph.getOrderedUpdatedNodes(o, true);
-    
-    if (!nodes.isEmpty() || paint.getForceDraw()) {
-    	Iterator<Node> nodIt = nodes.iterator();
+	
+    if (paint.getForceDraw() || graph.checkIfNodesCauseUpdate(o) || graph.checkIfEdgesCauseUpdate(o)) {
     	Graphics image = paint.getGraphics();
         Rectangle i = image.getClipBounds();
         image.setColor(Color.BLACK);
         image.fillRect(i.x, i.y, i.width, i.height);
-        List<CommunityEdge> drawnEdges = new ArrayList<CommunityEdge>();
         
-        while(nodIt.hasNext()){
-          Node next = nodIt.next();
-          if(!next.isVisible()){
-            continue;
-          }
-          drawNode(next, o, image);
-          
-          Iterator<CommunityEdge> eit = next.getEdges();
-          while(eit.hasNext()) {
-        	  CommunityEdge e = eit.next();
-        	  
-        	  if (drawnEdges.contains(e))
-        		  continue;
-        	  
-        	  if (e.isConflictEdge()) {
-        		image.setColor(Color.RED);
-        	  } else if(e.getStart().getCommunity() == e.getEnd().getCommunity() && e.getStart().getCommunity() != -1) {
-        	    image.setColor(e.getColor(graph));
-        	  } else {
-        	    image.setColor(Color.WHITE);
-        	  }
-        	  drawConnection(e, o, image);
-        	  
-        	  drawnEdges.add(e);
-          }
-        }
+        drawEdges(image, o);
+        drawNodes(image, o);
     }
     
     paint.setFinished(true);
+  }
+  
+  private void drawNodes(Graphics image, Rectangle o) {
+	Iterator<Node> nodIt = graph.getNodesInBounds(o, true).iterator();
+	
+    while(nodIt.hasNext()){
+      Node next = nodIt.next();
+      if(!next.isVisible()){
+        continue;
+      }
+      drawNode(next, o, image);
+    }
+  }
+  
+  private void drawEdges(Graphics image, Rectangle o) {
+	  Iterator<CommunityEdge> eit = (Iterator<CommunityEdge>) graph.getEdgesInBounds(o).iterator();
+      while(eit.hasNext()) {
+    	  CommunityEdge e = eit.next();
+    	  
+    	  if (!e.getStart().isVisible() || !e.getEnd().isVisible())
+    		  continue;
+    	  
+    	  if (e.isConflictEdge()) {
+    		image.setColor(Color.RED);
+    	  } else if(e.getStart().getCommunity() == e.getEnd().getCommunity() && e.getStart().getCommunity() != -1) {
+    	    image.setColor(e.getColor(graph));
+    	  } else {
+    	    image.setColor(Color.WHITE);
+    	  }
+    	  drawConnection(e, o, image);
+      }
   }
   
   @Override
